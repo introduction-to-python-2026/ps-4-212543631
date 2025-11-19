23d4beca2125a21ec952e48a21325922786f3a97
212543631 <adi.shefer.43@gmail.com>
2025-11-19 19:48:38 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,12 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1
+    for i in formula[1:]:
+        if i.isdigit():
+            prefix = formula[:digit_location]
+            numeric_part = int(formula[digit_location:])
+            return (prefix , numeric_part)
+        digit_location += 1
+    prefix = formula
+    return (prefix , 1)
